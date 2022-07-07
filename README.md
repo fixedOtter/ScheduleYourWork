100538d8365f21253c62c583344ca8aef607c36f
fixedOtter <fo1152rc@go.minnstate.edu>
2022-07-06 21:49:17 -0500

    initial

---
+++ README.md
@@ -0,0 +1,30 @@
+# %INSERT TITLE HERE%
+%project description with human words%
+
+## About the project
+%more thorough project description with more human words%
+
+## Project Criteria
+
+- GIVEN I am using a daily planner to create a schedule
+- WHEN I open the planner
+- THEN the current day is displayed at the top of the calendar
+- WHEN I scroll down
+- THEN I am presented with time blocks for standard business hours
+- WHEN I view the time blocks for that day
+- THEN each time block is color-coded to indicate whether it is in the past, present, or future
+- WHEN I click into a time block
+- THEN I can enter an event
+- WHEN I click the save button for that time block
+- THEN the text for that event is saved in local storage
+- WHEN I refresh the page
+- THEN the saved events persist
+
+## Project mockup
+%mockup of the project or outline%
+
+## Screenshots!
+%read the title dummy%
+
+## Deployment of project
+%where to access or use the project%
---
+++ assets/js/script.js
@@ -0,0 +1,68 @@
+// waits for jquery to be ready and then runs the code
+$(document).ready(function(){
+  // defining variables
+  const $currentDayEl = $(`#currentDay`);
+  const DateTime = luxon.DateTime;
+  let stringTime = DateTime.local();
+
+  // sets the currentDay to the current day and time
+  $currentDayEl.text(`${stringTime.toLocaleString(DateTime.DATETIME_MED)}`);
+
+  //save data func
+  function saveData() {
+    // grabs the userinput from the clicked hour
+    let userInput = $(this).siblings(`.description`).val();
+    // grabs the time from the clicked hour
+    let time = $(this).parent().attr(`id`);
+    // saves the time and userInput to the localstorage
+    localStorage.setItem(time, userInput);
+  }
+
+  // click listener to save data to local storage
+  $(".saveBtn").on("click", saveData);
+
+  // i love for loops
+  // this bad boy iterates through the saved times and updates the descriptions
+  for (let i = 8; i < 19; i++) {
+    $(`#t${i} .description`).val(localStorage.getItem(`t${i}`));
+  }
+
+  function timeMain() {
+    // grabs just the current hour
+    let currentHour = stringTime.toFormat(`H`);
+    
+    // runs a function on each time-block element
+    $(`.time-block`).each(function() {
+      // declares the time of the current time-block element
+      let calTime = parseInt($(this).attr(`id`).split(`t`)[1]);
+
+      console.log(calTime, currentHour);
+  
+      // checks and sets if past elements
+      if (calTime < currentHour) {
+        $(this).addClass("past");
+        $(this).removeClass("present");
+        $(this).removeClass("future");
+      // checks and sets current element
+      } else if (calTime === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+        $(this).removeClass("future");
+      // sets future elements
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    })
+  }
+
+
+  timeMain();
+  // testing to make sure it's grabbing the correct local time
+  // console.log(currentTime.toLocaleString(DateTime.DATETIME_MED));
+
+
+}); 
+
+
